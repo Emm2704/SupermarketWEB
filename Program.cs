739f05b0e3ce3daca465931f527c1499c9852e4f
Emmanuel Valencia <emmmanuelvalencia479gmail.com>
2023-11-18 11:06:18 -0500

    message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+
 namespace SupermarketWEB
 {
     public class Program
@@ -8,6 +11,12 @@ namespace SupermarketWEB
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            //agregando el copntexto  SupermarketContext a la aplicacion
+            builder.Services.AddDbContext<SupermarketContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("supermarketDB"))
+            );
+
 
             var app = builder.Build();
 
